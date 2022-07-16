412829353b0859b941ce1a7d2a4389cb584b4c9e
Shreya <shreyaparker317@gmail.com>
2022-07-16 13:58:51 +0530

    Navigation setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import "./App.css"
+import {BrowserRouter as Router,Routes , Route ,Link} from "react-router-dom";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+import Login from "./pages/Login";
 
+const App = () => {
+    return (
+        <Router>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/createpost">Create Post</Link>
+                <Link to="/login">Login</Link>
+            </nav>
+            <Routes>
+                <Route path ="/" element={<Home/>} />
+                <Route path ="/createpost" element={<CreatePost/>} />
+                <Route path ="/login" element={<Login/>} />
 
-
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
-}
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
---
+++ src/pages/CreatePost.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CreatePost = () => {
+    return (
+        <div>
+            crp
+        </div>
+    );
+};
+
+export default CreatePost;
