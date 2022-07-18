79d3718716e1c68a5da6d6a9f16e622bdfbcf03e
Shreya <shreyaparker317@gmail.com>
2022-07-18 23:19:05 +0530

    Displaying Blog posts

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import {signOut} from "firebase/auth"
 import {auth} from "./firebase-config";
 
 const App = () => {
-    const [isAuth,setIsAuth] =useState(false)
+    const [isAuth,setIsAuth] =useState(localStorage.getItem("isAuth"));
 
     const signUserOut =()=>{
         signOut(auth).then(()=>{
@@ -29,7 +29,7 @@ const App = () => {
                     )}
             </nav>
             <Routes>
-                <Route path ="/" element={<Home/>} />
+                <Route path ="/" element={<Home isAuth={isAuth}/>} />
                 <Route path ="/createpost" element={<CreatePost isAuth={isAuth}/>} />
                 <Route path ="/login" element={<Login setIsAuth={setIsAuth}/>} />
 
